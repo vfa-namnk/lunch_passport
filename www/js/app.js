101d938e4181bba388520757e36b2074eade72ef
NCMB <dinh.thuy.duong@nifty.co.jp>
2015-12-04 19:46:48 +0900

    Js sdk applied code

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,7 @@
 var appKey    = "YOUR_APP_KEY";
 var clientKey = "YOUR_CLIENT_KEY";
 var applicationID = "YOUR_APP_ID";
+var ncmb = new NCMB(appKey,clientKey);
 
 ///// Called when app launch
 $(function() {
@@ -8,7 +9,6 @@ $(function() {
   $("#RegisterBtn").click(onRegisterBtn);
   $("#YesBtn_logout").click(onLogoutBtn);
   $("#UseBtn").click(onUseBtn);
-  NCMB.initialize(appKey, clientKey);
 });
 
 //----------------------------------会員管理-----------------------------------//
@@ -36,11 +36,9 @@ function onLogoutBtn()
 var onSuccess = function(position){
     var location = { lat: position.coords.latitude, lng: position.coords.longitude};
     //mobile backendに登録しているストアを取得し、地図で表示
-    var ShopClass = NCMB.Object.extend("Shop");
-    var query = new NCMB.Query(ShopClass);
+    var ShopClass = ncmb.DataStore("Shop");
     //位置情報をもとに検索する条件を設定
-    var geoPoint = new NCMB.GeoPoint(location.lat, location.lng);
-    query.withinKilometers("geolocation", geoPoint, 5);
+    var geoPoint = new ncmb.GeoPoint(location.lat, location.lng);
     var mapOptions = {
                        center: location,
                        zoom: 14
@@ -49,31 +47,31 @@ var onSuccess = function(position){
     //現在地を地図に追加
     markToMap("現在地", location, map, null);
     //mobile backend上のデータ検索を実行する
-    query.find({
-        success: function(shops) {      
-            // 検索が成功した場合の処理
-            for (var i = 0; i < shops.length; i++){
-                var shop = shops[i];
-                var shopLocation = shop.get("geolocation");              
-                var myLatlng = new google.maps.LatLng(shopLocation.latitude, shopLocation.longitude);
-                var detail = "";
-                var shopName = shop.get("name");
-                detail += "<h2>"+ shopName +"</h2>";
-                var shopLocation = shop.get("geolocation");
-                var shopCoupon = shop.get("provideCoupon");
-                var shopLatLng = new google.maps.LatLng(shopLocation.latitude,shopLocation.longitude);
-                var locationLatLng = new google.maps.LatLng(location.lat,location.lng);
-                var distance = Math.round(google.maps.geometry.spherical.computeDistanceBetween (locationLatLng, shopLatLng));  
-                detail += "<p>距離: "+ distance + "(m)</p>";
-                detail += '<button onclick="showShop(\'' + shop.id + '\');">お店を見る</button>';
-                markToMap(detail, myLatlng, map, 'images/marker.png');     
-            }
-        },
-        error: function(error) {
-            // 検索に失敗した場合の処理
-            alert(error.message);
-        }
-    });
+    ShopClass.withinKilometers("geolocation", geoPoint, 5)
+             .fetchAll()
+             .then(function(shops) { 
+                // 検索が成功した場合の処理
+                for (var i = 0; i < shops.length; i++){
+                    var shop = shops[i];
+                    var shopLocation = shop.get("geolocation");              
+                    var myLatlng = new google.maps.LatLng(shopLocation.latitude, shopLocation.longitude);
+                    var detail = "";
+                    var shopName = shop.get("name");
+                    detail += "<h2>"+ shopName +"</h2>";
+                    var shopLocation = shop.get("geolocation");
+                    var shopCoupon = shop.get("provideCoupon");
+                    var shopLatLng = new google.maps.LatLng(shopLocation.latitude,shopLocation.longitude);
+                    var locationLatLng = new google.maps.LatLng(location.lat,location.lng);
+                    var distance = Math.round(google.maps.geometry.spherical.computeDistanceBetween (locationLatLng, shopLatLng));  
+                    detail += "<p>距離: "+ distance + "(m)</p>";
+                    detail += '<button onclick="showShop(\'' + shop.objectId + '\');">お店を見る</button>';
+                    markToMap(detail, myLatlng, map, 'images/marker.png');     
+                }
+             })
+             .catch(function(error) {
+                // 検索に失敗した場合の処理
+                alert(error.message);
+             });
 };
 
 //位置情報取得に失敗した場合のコールバック
@@ -106,34 +104,32 @@ function showMap(){
 //----------------------------------利用状況の表示-------------------------------------//
 
 function getShopDetail(shopId) {   
-    var ShopClass = NCMB.Object.extend("Shop");
-    var query = new NCMB.Query(ShopClass);
-    query.get(shopId, {
-        success: function(shop) {
+    var ShopClass = ncmb.DataStore("Shop");
+    ShopClass.fetchById(shopId)
+        .then(function(shop) {
             $("#shopName").text(shop.get("name"));
             $("#shopCapacity").text("スペース：" + shop.get("capacity") + "席");
             $("#shopImage").attr("src" , "https://mb.api.cloud.nifty.com/2013-09-01/applications/" + applicationID + "/publicFiles/" + shop.get("image"));
-            var UseClass = NCMB.Object.extend("Used");
-            var query = new NCMB.Query(UseClass);
-            query.equalTo("shop", shop);
-            query.equalTo("user", currentLoginUser);
-            query.count({
-              success: function(count) {
-                  if(count>0) {
-                    $("#shopUsage").text("今まで" + count + "回で利用いただきました。" );
-                  } else {
-                    $("#shopUsage").text("まだ利用していません。" );   
-                  }
-              },
-              error: function(error) {
-                // エラー
-              }
-            });    
-        },
-        error: function(error) {
+            var UseClass = ncmb.DataStore("Used");
+            UseClass
+                    .equalTo("shop", shopId)
+                    .equalTo("user", currentLoginUser.objectId)
+                    .count()
+                    .fetchAll()
+                    .then(function(results) {
+                          if(results.count>0) {
+                            $("#shopUsage").text("今まで" + results.count + "回で利用いただきました。" );
+                          } else {
+                            $("#shopUsage").text("まだ利用していません。" );   
+                          }
+                    })
+                    .catch(function(error) {
+                        // エラー
+                    });    
+        })
+        .catch(function(error) {
             alert(error.message);
-        }
-    });          
+        });          
 }
 
 function showShop(shopId) {
@@ -143,24 +139,18 @@ function showShop(shopId) {
 }
 
 function onUseBtn() {
-    var ShopClass = NCMB.Object.extend("Shop");
-    var shop = new ShopClass();
-    shop.set("objectId",currentShopId);  
-    var user = new NCMB.User();
-    user.id = currentLoginUser.id;
-    var UsedClass = NCMB.Object.extend("Used");
-    var used = new UsedClass();
-    used.set("shop", shop);
-    used.set("user", user);
-    used.save(null, {
-      success: function(obj) {
-        // 保存完了後に実行される
-        alert("利用登録完了！");
-        getShopDetail(currentShopId);
-      },
-      error: function(obj, error) {
-        // エラー時に実行される
-        alert("登録失敗！次のエラーが発生：" + error.message);
-      }
-    });
+    var UseClass = ncmb.DataStore("Used");
+    var used = new UseClass();
+    used.set("shop", currentShopId)
+        .set("user", currentLoginUser.objectId)
+        .save()
+        .then(function(obj) {
+            // 保存完了後に実行される
+            alert("利用登録完了！");
+            getShopDetail(currentShopId);
+        })
+        .catch(function(error) {
+            // エラー時に実行される
+            alert("登録失敗！次のエラーが発生：" + error.message);
+        });
 }
